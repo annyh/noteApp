9549a5b099019aa18c20bcaf611759aae3f090e4
Anny He <anny.he@salesforce.com>
2018-03-31 13:21:34 -0700

    modify color span

--- src/Note.js
+++ src/Note.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Item = styled.div`
-  background-color: black;
   box-sizing: border-box;
   padding: 20px;
   flex: 2;
-  color: #fff;
-  border: 1px solid white;
+  color: #000;
+  border: 1px solid black;
+
+  border-top: ${ (p) => p.backgroundColor ? '10px ' + p.backgroundColor + ' solid' : '10px red solid' };
+  border-radius: 10px;
 
   @media screen and (max-width:1000px) {
     flex-basis: 50%;
@@ -18,13 +20,16 @@ const Item = styled.div`
   }
 `;
 
-const Colored = styled.span`
+const Colored = styled.button`
+  width: 25px;
+  height: 25px;
   background: ${ (p) => p.backgroundColor };
 `;
 
 module.exports = class Note extends React.Component {
   constructor(props) {
     super(props);
+    this.setColor = this.setColor.bind(this);
     this.state = {
       color: 'red',
       showModal: false,
@@ -32,14 +37,24 @@ module.exports = class Note extends React.Component {
     };
   }
 
+  setColor(event) {
+    const color = event.target.name;
+    this.setState({
+      color,
+    });
+  }
+
   render() {
     const { title, text } = this.props;
     const colors = ['red', 'yellow', 'green', 'blue'];
 
-    return <Item>
+    return <Item backgroundColor={ this.state.color }>
       <div>
       { colors.map((color) => <Colored
-        key={ color } backgroundColor={ color }>{ color }</Colored>) }
+        onClick={ this.setColor }
+        key={ color }
+        name={ color }
+        backgroundColor={ color } />) }
       </div>
       <h2>{ title }</h2>
       <p>{ text }</p>
